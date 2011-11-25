e096dd2d4dc7dd4a71e710833d73d6f4bb99a83d
Brian Schroeder <abductee@gmail.com>
2011-11-25 01:22:05 -0800

    adding some funny on-click behavior

---
+++ kevun.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+    var numbah = 0;
+    $('#kevun').click( function() {
+        if(numbah == 0) {
+            numbah = 1;
+        } else {
+            numbah = 0;
+        }
+
+        $('html').css('background').('url(kevun' + numbah + '.jpg) no-repeat center center fixed');
+
+    
+    }
+
+});
